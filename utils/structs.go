f40c14123ac7c0bb4c45e374d7aa9788915f079f
KowsalyaMahendran <kowsalya.mahendran@jaam.co>
2024-08-23 17:16:43 +0530

    struct change

--- utils/structs.go
+++ utils/structs.go
@@ -1,5 +1,9 @@
 package utils
 
 type User struct {
-	UserName string `json:"userName"`
+	UserId      string `json:"user_id" gorm:"primaryKey;column:user_id"`
+	UserName    string `json:"userName" gorm:"column:userName"`
+	UserRole    string `json:"userRole" gorm:"column:userRole"`
+	UserEmail   string `json:"userEmail" gorm:"column:userEmail"`
+	CreatedDate string `json:"createdDate" gorm:"column:createdDate"`
 }
